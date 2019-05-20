18dca4cd7a2e552ee8a835b43b28f3bb043bf76c
ilqar ilyasov <ilqar_ilyasov@yahoo.com>
2019-05-20 16:11:38 -0500

    Create New Task popover view

---
+++ Fairhsare/Fairshare/Views/NewTaskPopoverView.swift
@@ -0,0 +1,17 @@
+//
+//  NewTaskPopoverView.swift
+//  Fairshare
+//
+//  Created by Ilgar Ilyasov on 5/20/19.
+//  Copyright © 2019 Lambda School Labs. All rights reserved.
+//
+
+import Foundation
+
+class NewTaskPopoverView: UIView, NibInstantiatable {
+    
+    static var nibName: NibName = "NewTaskPopoverView"
+    
+
+
+}
