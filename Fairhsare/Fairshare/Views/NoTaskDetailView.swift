f8248b257a9481115626bf2265cfa7dbe5d55fab
ilqar ilyasov <ilqar_ilyasov@yahoo.com>
2019-05-22 16:38:09 -0500

    Add NoTaskDetailView

---
+++ Fairhsare/Fairshare/Views/NoTaskDetailView.swift
@@ -0,0 +1,17 @@
+//
+//  NoTaskDetailView.swift
+//  Fairshare
+//
+//  Created by Ilgar Ilyasov on 5/22/19.
+//  Copyright © 2019 Lambda School Labs. All rights reserved.
+//
+
+import Foundation
+
+class NoTaskDetailView: UIView, NibInstantiatable {
+    
+    static var nibName: NibName = "NoTaskDetailView"
+    
+    
+
+}
